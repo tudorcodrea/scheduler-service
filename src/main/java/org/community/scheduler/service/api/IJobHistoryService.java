00e858a6a49193849bcede86fae0ef8ede54eaee
tudorcodrea <tudor.codrea@softvision.com>
2021-01-08 09:57:56 +0200

    Initial Commit

---
+++ src/main/java/org/community/scheduler/service/api/IJobHistoryService.java
@@ -0,0 +1,20 @@
+package org.community.scheduler.service.api;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.community.scheduler.entity.SchedulerJobHistory;
+
+/**
+ * @author tudor.codrea
+ *
+ */
+public interface IJobHistoryService {
+
+	List<SchedulerJobHistory> getAll();
+	
+	Optional<SchedulerJobHistory> getLastRunByName(String jobName);
+
+	SchedulerJobHistory insert(SchedulerJobHistory sjh);
+	SchedulerJobHistory update(SchedulerJobHistory sjh);
+}
